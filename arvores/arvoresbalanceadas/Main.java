94234cd5bf13d2dae1f3fc373eb9287a1f800e85
esteroliver <oliver.ester.2004@gmail.com>
2024-11-11 10:56:58 -0300

    feat: classe main

---
+++ arvores/arvoresbalanceadas/Main.java
@@ -0,0 +1,17 @@
+class Main{
+    public static void main(String[] args){
+        No node1 = new No(20);
+        No node2 = new No(30);
+        No node3 = new No(15);
+        No node4 = new No(33);
+        No node5 = new No(24);
+
+        ArvoreAvl arvore = new ArvoreAvl();
+
+        arvore.inserirNo(node1);
+        arvore.inserirNo(node2);
+        arvore.inserirNo(node3);
+        arvore.inserirNo(node4);
+        arvore.inserirNo(node5);
+    }
+}
